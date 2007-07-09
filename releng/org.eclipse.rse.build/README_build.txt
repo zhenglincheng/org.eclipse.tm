08bb3e96f46507a2d11d1da03c6afb49274558fa
cvs2svn <tm-dev@eclipse.org>
2007-07-09 10:25:16 +0000

    This commit was manufactured by cvs2svn to create tag 'R2_0_0_1'.
    Sprout from master 2007-06-28 16:51:25 UTC Martin Oberhuber < martin.oberhuber@windriver.com> 'Update global tags for R2_0 in TM Repository'
    Cherrypick from master 2007-07-06 14:38:29 UTC Martin Oberhuber < martin.oberhuber@windriver.com> 'Rename 2.0a --> 2.0.0.1':
        rse/features/org.eclipse.rse.core-patch/.project
        rse/features/org.eclipse.rse.core-patch/build.properties
        rse/features/org.eclipse.rse.core-patch/epl-v10.html
        rse/features/org.eclipse.rse.core-patch/feature.properties
        rse/features/org.eclipse.rse.core-patch/feature.xml
        rse/features/org.eclipse.rse.core-patch/license.html
        rse/features/org.eclipse.rse.ftp-patch/.project
        rse/features/org.eclipse.rse.ftp-patch/build.properties
        rse/features/org.eclipse.rse.ftp-patch/epl-v10.html
        rse/features/org.eclipse.rse.ftp-patch/feature.properties
        rse/features/org.eclipse.rse.ftp-patch/feature.xml
        rse/features/org.eclipse.rse.ftp-patch/license.html
        rse/plugins/org.eclipse.rse.services/clientserver/org/eclipse/rse/services/clientserver/archiveutils/SystemZipHandler.java
    Cherrypick from master 2007-02-22 22:38:26 UTC Martin Oberhuber < martin.oberhuber@windriver.com> '[cleanup] fix copyright year 2007':
        rse/doc/org.eclipse.rse.doc.user/ref/rrsepref.html
    Cherrypick from master 2007-07-03 09:50:03 UTC Martin Oberhuber < martin.oberhuber@windriver.com> 'Switch to 2.0 Stream Maintenance Builds':
        releng/org.eclipse.rse.build/downloads/dlconfig.txt
    Cherrypick from R2_0_patches 2007-07-09 10:25:15 UTC Martin Oberhuber < martin.oberhuber@windriver.com> 'Fix patch site.xml generation':
        releng/org.eclipse.rse.build/README_build.txt
        releng/org.eclipse.rse.build/bin/doit_irsbuild.sh
        releng/org.eclipse.rse.build/bin/doit_nightly.sh
        releng/org.eclipse.rse.build/customTargets.xml
        releng/org.eclipse.rse.build/maps/rse.map
        releng/org.eclipse.rse.build/setup.sh
        releng/org.eclipse.rse.build/template/buildNotes.php
        releng/org.eclipse.rse.updatesite/bin/mkTestUpdates.sh
        releng/org.eclipse.rse.updatesite/bin/sign.sh
        releng/org.eclipse.rse.updatesite/index.html
        releng/org.eclipse.rse.updatesite/site.xml
        releng/org.eclipse.rse.updatesite/web/site.xsl
    Delete:
        rse/features/org.eclipse.rse.efs-feature/.project
        rse/features/org.eclipse.rse.efs-feature/build.properties
        rse/features/org.eclipse.rse.efs-feature/eclipse_update_120.jpg
        rse/features/org.eclipse.rse.efs-feature/epl-v10.html
        rse/features/org.eclipse.rse.efs-feature/feature.properties
        rse/features/org.eclipse.rse.efs-feature/feature.xml
        rse/features/org.eclipse.rse.efs-feature/license.html
        rse/features/org.eclipse.rse.efs-feature/sourceTemplatePlugin/about.html
        rse/features/org.eclipse.rse.efs-feature/sourceTemplatePlugin/build.properties
        rse/features/org.eclipse.rse.efs-feature/sourceTemplatePlugin/plugin.properties
        rse/plugins/org.eclipse.rse.eclipse.filesystem/.project
        rse/plugins/org.eclipse.rse.eclipse.filesystem/moved_project.txt
        rse/plugins/org.eclipse.rse.logging/.project
        rse/plugins/org.eclipse.rse.logging/decommissioned.txt

--- releng/org.eclipse.rse.build/README_build.txt
+++ releng/org.eclipse.rse.build/README_build.txt
@@ -7,9 +7,9 @@ ssh build.eclipse.org
 cd /shared/dsdp/tm/
 mkdir ws2_user
 cd ws2_user
-ln -s `pws` $HOME/ws2
+ln -s `pws` $HOME/ws2_0_patches
 ln -s /home/data/httpd/download.eclipse.org/dsdp/tm $HOME/downloads-tm
-cp -R ../ws2/IBMJava2-ppc-142 .
+cp -R ../ws2_0_patches/IBMJava2-ppc-142 .
 wget -O setup.sh "http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.tm.rse/releng/org.eclipse.rse.build/setup.sh?rev=HEAD&cvsroot=DSDP_Project&content-type=text/plain"
 chmod a+x setup.sh
 ./setup.sh
@@ -17,7 +17,7 @@ chmod a+x setup.sh
 
 2. Do an N-build
 ----------------
-cd $HOME/ws2
+cd $HOME/ws2_0_patches
 ./doit_nightly.sh
 
 
@@ -31,7 +31,7 @@ Right-click > Team > Release...
 Select Mapfile "org.eclipse.rse.build"
 Press next ... tag and commit the Mapfiles
 ssh build.eclipse.org
-cd ws2
+cd ws2_0_patches
 ./doit_irsbuild.sh I
 When build worked ok, tag org.eclipse.rse.build: e.g. I20070605-1200
 
@@ -40,19 +40,19 @@ When build worked ok, tag org.eclipse.rse.build: e.g. I20070605-1200
 Just like I-build, but also update 
    org.eclipse.rse.build/template/buildNotes.php
 ssh build.eclipse.org
-cd ws2
+cd ws2_0_patches
 ./doit_irsbuild.sh S 2.0RC3
 
 3b) Promote an S-build to official
 ----------------------------------
 After testing the "invisible" S-build:
 ssh build.eclipse.org
-cd ws2/publish/S-2.0RC3*
+cd ws2_0_patches/publish/S-2.0RC3*
 mv package.count.orig package.count
 
 3c) Promote an S-build to Europa
 --------------------------------
-After S-build has been prepared (on signedUpdates)
+After S-build has been prepared (on signedPatchUpdates)
 On local Eclipse client, checkout Europa projects according to
     http://wiki.eclipse.org/index.php/Europa_Build
 Open file 
@@ -62,8 +62,8 @@ cd downloads-tm/updates/milestones
 rm -rf features.prev plugins.prev
 mv features features.prev
 mv plugins plugins.prev
-cp -R ../../signedUpdates/features .
-cp -R ../../signedUpdates/plugins .
+cp -R ../../signedPatchUpdates/features .
+cp -R ../../signedPatchUpdates/plugins .
 cd bin
 ./mkTestUpdates.sh
 From the shell where the build is ongoing, copy & Paste the 
--- releng/org.eclipse.rse.build/setup.sh
+++ releng/org.eclipse.rse.build/setup.sh
@@ -14,14 +14,14 @@
 # Works on build.eclipse.org -- may need to be adjusted
 # for other hosts.
 #
-# This must be run in $HOME/ws2 in order for the mkTestUpdateSite.sh
+# This must be run in $HOME/ws2_0_patches in order for the mkTestUpdateSite.sh
 # script to find the published packages
 #
 # Bootstrapping: Get this script by
 # wget -O setup.sh "http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.tm.rse/releng/org.eclipse.rse.build/setup.sh?rev=HEAD&cvsroot=DSDP_Project&content-type=text/plain"
 # sh setup.sh
 # ./doit_ibuild.sh
-# cd testUpdates/bin
+# cd testPatchUpdates/bin
 # mkTestUpdates.sh
 
 curdir=`pwd`
@@ -41,32 +41,33 @@ case ${uname_s}${uname_m} in
 esac
 
 # prepare the base Eclipse installation in folder "eclipse"
-if [ ! -f eclipse/plugins/org.eclipse.core.resources_3.3.0.v20070530.jar ]; then
+if [ ! -f eclipse/plugins/org.eclipse.swt_3.3.0.v3346.jar ]; then
   curdir2=`pwd`
   if [ ! -d eclipse -o -h eclipse ]; then
-    if [ -d eclipse-3.3RC3-${ep_arch} ]; then
-      rm -rf eclipse-3.3RC3-${ep_arch}
+    if [ -d eclipse-3.3-${ep_arch} ]; then
+      rm -rf eclipse-3.3-${ep_arch}
     fi
-    mkdir eclipse-3.3RC3-${ep_arch}
-    cd eclipse-3.3RC3-${ep_arch}
+    mkdir eclipse-3.3-${ep_arch}
+    cd eclipse-3.3-${ep_arch}
   else
     rm -rf eclipse
   fi
-  ## Eclipse Platform 3.3RC2
-  #wget "http://download.eclipse.org/eclipse/downloads/drops/S-3.3RC3-200706011539/eclipse-platform-3.3RC3-${ep_arch}.tar.gz"
-  #tar xfvz eclipse-platform-3.3RC3-${ep_arch}.tar.gz
-  #rm eclipse-platform-3.3RC3-${ep_arch}.tar.gz
-  # Eclipse SDK 3.3: Need the SDK because EMF needs JDT (somehow)
+  ## Eclipse Platform 3.3
+  #wget "http://download.eclipse.org/eclipse/downloads/drops/R-3.3-200706251500/eclipse-platform-3.3-${ep_arch}.tar.gz"
+  #tar xfvz eclipse-platform-3.3-${ep_arch}.tar.gz
+  #rm eclipse-platform-3.3-${ep_arch}.tar.gz
+  # Eclipse SDK 3.3: Need the SDK so we can link into docs
   echo "Getting Eclipse SDK..."
-  wget "http://download.eclipse.org/eclipse/downloads/drops/S-3.3RC3-200706011539/eclipse-SDK-3.3RC3-${ep_arch}.tar.gz"
-  tar xfvz eclipse-SDK-3.3RC3-${ep_arch}.tar.gz
-  rm eclipse-SDK-3.3RC3-${ep_arch}.tar.gz
+  wget "http://download.eclipse.org/eclipse/downloads/drops/R-3.3-200706251500/eclipse-SDK-3.3-${ep_arch}.tar.gz"
+  echo "Extracting eclipse-SDK..."
+  tar xfz eclipse-SDK-3.3-${ep_arch}.tar.gz
+  rm eclipse-SDK-3.3-${ep_arch}.tar.gz
   cd "${curdir2}"
   if [ ! -d eclipse -o -h eclipse ]; then
     if [ -e eclipse ]; then 
       rm eclipse
     fi
-    ln -s eclipse-3.3RC3-${ep_arch}/eclipse eclipse
+    ln -s eclipse-3.3-${ep_arch}/eclipse eclipse
   fi
 fi
 if [ ! -f eclipse/startup.jar ]; then
@@ -84,31 +85,71 @@ if [ ! -f eclipse/startup.jar ]; then
   fi
   cd ${curdir2}
 fi
-if [ ! -f eclipse/plugins/org.eclipse.cdt.core_4.0.0.200705251830.jar ]; then
+if [ ! -f eclipse/plugins/org.eclipse.cdt.core_4.0.0.200706261300.jar ]; then
   # CDT 4.0.0 Runtime
   echo "Getting CDT Runtime..."
-  wget "http://download.eclipse.org/tools/cdt/releases/europa/dist/4.0.0RC2/cdt-4.0.0-RC2-${cdt_arch}.tar.gz"
-  tar xfvz cdt-4.0.0-RC2-${cdt_arch}.tar.gz
-  rm cdt-4.0.0-RC2-${cdt_arch}.tar.gz
+  CDT_OK=0
+  wget "http://download.eclipse.org/tools/cdt/releases/europa/dist/cdt-master-4.0.0.zip"
+  #java \
+  #  -classpath eclipse/plugins/org.eclipse.help.base_3.3.0.v20070606.jar \
+  CDTTMP=`pwd`/tmp.$$
+  mkdir ${CDTTMP}
+  cd ${CDTTMP}
+  echo "Extracting CDT-master..."
+  unzip -q ../cdt-master-4.0.0.zip
+  cd ..
+  CMD="java -jar eclipse/plugins/org.eclipse.equinox.launcher_1.0.0.v20070606.jar"
+  CMD="${CMD} -application org.eclipse.update.core.standaloneUpdate"
+  LCMD="${CMD} -command search -from file://${CDTTMP}"
+  echo ""
+  echo ${LCMD}
+  result=`${LCMD} | grep 'org.eclipse.cdt 4'`
+  rc=$?
+  echo "rc=${rc}"
+  echo $result
+  if [ "${rc}" = "0" ]; then
+    VERSION=`echo ${result} | sed -e 's,".*org\.eclipse\.cdt ,,'`
+    echo "VERSION=${VERSION}"
+    UCMD="${CMD} -command install"
+    UCMD="${UCMD} -featureId org.eclipse.cdt"
+    UCMD="${UCMD} -version 4.0.0.200706261300"
+    UCMD="${UCMD} -from file://${CDTTMP}"
+    echo ""
+    echo ${UCMD}
+    ${UCMD}
+    rc=$?
+    echo "rc=${rc}"
+    if [ "${rc}" = "0" ]; then
+      rm -rf ${CDTTMP}
+      rm cdt-master-4.0.0.zip
+      CDT_OK=1
+    fi
+  fi
+  if [ ${CDT_OK} != 1 ]; then
+    echo "CDT Install failed!"
+    exit 1
+  fi
 fi
-if [ ! -f eclipse/plugins/org.eclipse.emf.doc_2.3.0.v200705300200/doc.zip ]; then
+if [ ! -f eclipse/plugins/org.eclipse.emf.doc_2.3.0.v200706262000/doc.zip ]; then
   # Need EMF 2.3.0 SDK for Service Discovery ISV Docs Backlinks
   echo "Getting EMF SDK..."
-  wget "http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.3.0/S200705300200/emf-sdo-xsd-SDK-2.3.0RC2.zip"
-  unzip -o emf-sdo-xsd-SDK-2.3.0RC2.zip
-  rm emf-sdo-xsd-SDK-2.3.0RC2.zip 
+  wget "http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.3.0/R200706262000/emf-sdo-xsd-SDK-2.3.0.zip"
+  echo "Extracting EMF..."
+  unzip -q -o emf-sdo-xsd-SDK-2.3.0.zip
+  rm emf-sdo-xsd-SDK-2.3.0.zip 
 fi
-if [ ! -f eclipse/plugins/org.junit_3.8.2.v200705250844/junit.jar ]; then
+if [ ! -f eclipse/plugins/org.junit_3.8.2.v200706111738/junit.jar ]; then
   # Eclipse Test Framework
   echo "Getting Eclipse Test Framework..."
-  wget "http://download.eclipse.org/eclipse/downloads/drops/S-3.3RC3-200706011539/eclipse-test-framework-3.3RC3.zip"
-  unzip -o eclipse-test-framework-3.3RC3.zip
-  rm eclipse-test-framework-3.3RC3.zip
+  wget "http://download.eclipse.org/eclipse/downloads/drops/R-3.3-200706251500/eclipse-test-framework-3.3.zip"
+  echo "Extracting eclipse-test-framework..."
+  unzip -q -o eclipse-test-framework-3.3.zip
+  rm eclipse-test-framework-3.3.zip
 fi
 
 # checkout the basebuilder
-baseBuilderTag=RC3_33
-if [ ! -f org.eclipse.releng.basebuilder/plugins/org.eclipse.pde.core_3.3.0.v20070531-0010.jar \
+baseBuilderTag=RC4_33
+if [ ! -f org.eclipse.releng.basebuilder/plugins/org.eclipse.pde.core_3.3.0.v20070608-1300.jar \
   -o ! -f org.eclipse.releng.basebuilder/plugins/org.eclipse.pde.build/pdebuild.jar ]; then
   if [ -d org.eclipse.releng.basebuilder ]; then
     echo "Re-getting basebuilder from CVS..."
@@ -138,7 +179,7 @@ fi
 if [ -f org.eclipse.rse.build/CVS/Entries ]; then
   echo "Updating org.eclipse.rse.build from CVS"
   cd org.eclipse.rse.build
-  cvs -q update -dPR
+  cvs -q update -dPR -r R2_0_patches
   cd ..
 else
   if [ -d org.eclipse.rse.build ]; then
@@ -147,7 +188,7 @@ else
   else
     echo "Getting org.eclipse.rse.build from CVS"
   fi
-  cvs -q -d :pserver:anonymous@dev.eclipse.org:/cvsroot/dsdp co -Rd org.eclipse.rse.build org.eclipse.tm.rse/releng/org.eclipse.rse.build
+  cvs -q -d :pserver:anonymous@dev.eclipse.org:/cvsroot/dsdp co -R -r R2_0_patches -d org.eclipse.rse.build org.eclipse.tm.rse/releng/org.eclipse.rse.build
 fi
 
 # prepare directories for the build
@@ -162,9 +203,9 @@ if [ ! -d publish ]; then
   D=/home/data/httpd/download.eclipse.org/dsdp/tm/downloads/drops
   if [ -d ${D} ]; then ln -s ${D} publish; else mkdir publish; fi
 fi
-if [ ! -d testUpdates ]; then
-  D=/home/data/httpd/download.eclipse.org/dsdp/tm/testUpdates
-  if [ -d ${D} ]; then ln -s ${D} testUpdates; else mkdir testUpdates; fi
+if [ ! -d testPatchUpdates ]; then
+  D=/home/data/httpd/download.eclipse.org/dsdp/tm/testPatchUpdates
+  if [ -d ${D} ]; then ln -s ${D} testPatchUpdates; else mkdir testPatchUpdates; fi
 fi
 if [ ! -d updates ]; then
   D=/home/data/httpd/download.eclipse.org/dsdp/tm/updates
@@ -184,7 +225,7 @@ if [ ! -h doit_nightly.sh ]; then
 fi
 if [ ! -h setup.sh ]; then
   if [ -f setup.sh ]; then rm -f setup.sh; fi
-  ln -s org.eclipse.rse.build/bin/setup.sh .
+  ln -s org.eclipse.rse.build/setup.sh .
 fi
 chmod a+x doit_irsbuild.sh doit_nightly.sh
 cd org.eclipse.rse.build
@@ -195,11 +236,11 @@ echo "Your build environment is now created."
 echo ""
 echo "Run \"./doit_irsbuild.sh I\" to create an I-build."
 echo ""
-echo "Test the testUpdates, then copy them to updates:"
+echo "Test the testPatchUpdates, then copy them to updates:"
 echo "cd updates"
 echo "rm -rf plugins features"
-echo "cp -R ../testUpdates/plugins ."
-echo "cp -R ../testUpdates/features ."
+echo "cp -R ../testPatchUpdates/plugins ."
+echo "cp -R ../testPatchUpdates/features ."
 echo "cd bin"
 echo "cvs update"
 echo "./mkTestUpdates.sh"
