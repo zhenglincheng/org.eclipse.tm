a9f78a00764143f923204f9ca28f27b40b2bce22
David McKnight <dmcknigh@ca.ibm.com>
2010-04-22 14:32:30 +0000

    [309813] RSE permits opening of file after access removed

--- rse/plugins/org.eclipse.rse.files.ui/src/org/eclipse/rse/internal/files/ui/actions/SystemEditFilesAction.java
+++ rse/plugins/org.eclipse.rse.files.ui/src/org/eclipse/rse/internal/files/ui/actions/SystemEditFilesAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2008 IBM Corporation and others.
+ * Copyright (c) 2002, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,6 +16,7 @@
  * David McKnight   (IBM)        - [209660] check for changed encoding before using cached file
  * David McKnight   (IBM)        - [189873] DownloadJob changed to DownloadAndOpenJob
  * David McKnight   (IBM)        - [224377] "open with" menu does not have "other" option
+ * David McKnight   (IBM)        - [309813] RSE permits opening of file after access removed
  *******************************************************************************/
 
 package org.eclipse.rse.internal.files.ui.actions;
@@ -149,11 +150,14 @@ public class SystemEditFilesAction extends SystemBaseAction {
 			boolean usedBinary = properties.getUsedBinaryTransfer();
 			boolean isBinary = remoteFile.isBinary();
 			
+			boolean usedReadOnly = properties.getReadOnly();
+			boolean isReadOnly = !remoteFile.canWrite();
+			
 			return (!dirty && 
 					!remoteNewer && 
 					usedBinary == isBinary &&
+					usedReadOnly == isReadOnly && 
 					!encodingChanged);
-
 		}
 		return false;
 	}
