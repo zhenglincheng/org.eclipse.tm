6e735dfc181f0a11988c2bde75316462d631eece
Martin Oberhuber < martin.oberhuber@windriver.com>
2010-06-03 08:39:03 +0000

    Bug 315529 -  Backport [terminal] Terminal always jumps to line 1 if ScrollLock is activated

--- terminal/org.eclipse.tm.terminal/src/org/eclipse/tm/internal/terminal/textcanvas/TextLineRenderer.java
+++ terminal/org.eclipse.tm.terminal/src/org/eclipse/tm/internal/terminal/textcanvas/TextLineRenderer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 Wind River Systems, Inc. and others.
+ * Copyright (c) 2007, 2010 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  * Michael Scharf (Wind River) - initial API and implementation
  * Michael Scharf (Wind River) - [205260] Terminal does not take the font from the preferences
  * Michael Scharf (Wind River) - [206328] Terminal does not draw correctly with proportional fonts
+ * Anton Leherbauer (Wind River) - [294468] Fix scroller and text line rendering
  *******************************************************************************/
 package org.eclipse.tm.internal.terminal.textcanvas;
 
@@ -28,10 +29,8 @@ import org.eclipse.tm.terminal.model.Style;
 public class TextLineRenderer implements ILinelRenderer {
 	private final ITextCanvasModel fModel;
 	StyleMap fStyleMap=new StyleMap();
-	Color fBackgroundColor;
 	public TextLineRenderer(TextCanvas c, ITextCanvasModel model) {
 		fModel=model;
-		fBackgroundColor=Display.getCurrent().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
 	}
 	/* (non-Javadoc)
 	 * @see com.imagicus.thumbs.view.ICellRenderer#getCellWidth()
@@ -47,7 +46,7 @@ public class TextLineRenderer implements ILinelRenderer {
 	}
 	public void drawLine(ITextCanvasModel model, GC gc, int line, int x, int y, int colFirst, int colLast) {
 		if(line<0 || line>=getTerminalText().getHeight() || colFirst>=getTerminalText().getWidth() || colFirst-colLast==0) {
-			fillBackground(gc, x, y, getCellWidth()*(colFirst-colLast), getCellHeight());
+			fillBackground(gc, x, y, getCellWidth()*(colLast-colFirst), getCellHeight());
 		} else {
 			colLast=Math.min(colLast, getTerminalText().getWidth());
 			LineSegment[] segments=getTerminalText().getLineSegments(line, colFirst, colLast-colFirst);
@@ -87,15 +86,17 @@ public class TextLineRenderer implements ILinelRenderer {
 
 	private void fillBackground(GC gc, int x, int y, int width, int height) {
 		Color bg=gc.getBackground();
-		gc.setBackground(getBackgroundColor());
+		gc.setBackground(getDefaultBackgroundColor());
 		gc.fillRectangle (x,y,width,height);
 		gc.setBackground(bg);
 
 	}
 
-	private Color getBackgroundColor() {
-		return fBackgroundColor;
+	public Color getDefaultBackgroundColor() {
+		// null == default style
+		return fStyleMap.getBackgroundColor(null);
 	}
+	
 	private void drawCursor(ITextCanvasModel model, GC gc, int row, int x, int y, int colFirst) {
 		if(!model.isCursorOn())
 			return;
