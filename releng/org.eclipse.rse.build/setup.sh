e4b02218af09cead137bf154c8d991ac3101c774
Martin Oberhuber < martin.oberhuber@windriver.com>
2010-08-19 14:56:58 +0000

    [releng] Setup for 3.2.x M-builds

--- releng/org.eclipse.rse.build/setup.sh
+++ releng/org.eclipse.rse.build/setup.sh
@@ -173,8 +173,8 @@ fi
 
 # checkout the basebuilder
 baseBuilderTag=R36_RC4
-if [ ! -f org.eclipse.releng.basebuilder/plugins/org.eclipse.pde.core_3.6.0.v20100519.jar \
-  -o ! -f org.eclipse.releng.basebuilder/plugins/org.eclipse.pde.build_3.6.0.v20100512/pdebuild.jar \
+if [ ! -f org.eclipse.releng.basebuilder/plugins/org.eclipse.pde.core_3.6.0.v20100601.jar \
+  -o ! -f org.eclipse.releng.basebuilder/plugins/org.eclipse.pde.build_3.6.0.v20100603/pdebuild.jar \
   -o ! -f org.eclipse.releng.basebuilder/plugins/org.eclipse.equinox.p2.metadata.generator_1.0.200.v20100503a.jar ]; then
   if [ -d org.eclipse.releng.basebuilder ]; then
     echo "Re-getting basebuilder from CVS..."
