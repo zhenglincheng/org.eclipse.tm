53191886065e5dfe7fbfcc8555afd3c949d12a58
Martin Oberhuber < martin.oberhuber@windriver.com>
2010-09-07 11:15:18 +0000

    [releng] release Helios SR1 RC3

--- releng/org.eclipse.rse.updatesite/bin/mkTestUpdates.sh
+++ releng/org.eclipse.rse.updatesite/bin/mkTestUpdates.sh
@@ -163,6 +163,7 @@ elif [ ${TYPE} = testSigned ]; then
       #get jars from test32Updates, sign them and put them here
       mkdir ${SITE}/features.${stamp}
       mkdir -p ${STAGING}/updates.${stamp}/features
+      chmod -R g+w ${STAGING}/updates.${stamp}
       cp -R ${SITE}/../test32Updates/features/*.jar ${STAGING}/updates.${stamp}/features
       cd ${STAGING}/updates.${stamp}/features
       for x in `ls *.jar`; do
@@ -202,6 +203,7 @@ elif [ ${TYPE} = testSigned ]; then
         rmdir ${STAGING}/updates.${stamp}/features
         mkdir ${SITE}/plugins.${stamp}
         mkdir -p ${STAGING}/updates.${stamp}/plugins
+        chmod -R g+w ${STAGING}/updates.${stamp}
         cp ${SITE}/../test32Updates/plugins/*.jar ${STAGING}/updates.${stamp}/plugins
         cd ${STAGING}/updates.${stamp}/plugins
         for x in `ls *.jar`; do
