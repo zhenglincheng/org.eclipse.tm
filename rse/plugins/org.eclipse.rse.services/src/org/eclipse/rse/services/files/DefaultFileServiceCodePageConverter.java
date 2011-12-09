66a7b59d17239f6cc3f52b12b1b0372e7f4e15d7
David McKnight <dmcknigh@ca.ibm.com>
2011-12-09 21:50:25 +0000

    [280453] [performance] DefaultFileServiceCodePageConverter is wasteful with main memory

--- rse/plugins/org.eclipse.rse.services/src/org/eclipse/rse/services/files/DefaultFileServiceCodePageConverter.java
+++ rse/plugins/org.eclipse.rse.services/src/org/eclipse/rse/services/files/DefaultFileServiceCodePageConverter.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2007, 2010 IBM Corporation. All rights reserved.
+ * Copyright (c) 2007, 2011 IBM Corporation. All rights reserved.
  * This program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -14,6 +14,7 @@
  * David McKnight    (IBM)  -[246857] Rename problem when a file is opened in the editor
  * David McKnight    (IBM)  -[279014] [dstore][encoding] text file corruption can occur when downloading from UTF8 to cp1252
  * David McKnight    (IBM)  -[324669] [dstore] IBM-eucJP to UTF-8 char conversion appends nulls to end of file during text-mode download
+ * David McKnight    (IBM)  -[280453] [performance] DefaultFileServiceCodePageConverter is wasteful with main memory
  ********************************************************************************/
 package org.eclipse.rse.services.files;
 
@@ -53,33 +54,48 @@ public class DefaultFileServiceCodePageConverter implements
 		// read in the file
 		try
 		{
+			// decoder to go from remote encoding to UTF8
+			Charset rmtCharset = Charset.forName(remoteEncoding);
+			CharsetDecoder rmtDecoder = rmtCharset.newDecoder();
+
+			inputStream = new FileInputStream(file);
+			
 			int fileLength = (int)file.length();
+			BufferedInputStream bufInputStream = new BufferedInputStream(inputStream, fileLength);
+			
+
 			if (fileLength > 0){
-				inputStream = new FileInputStream(file);
-				BufferedInputStream bufInputStream = new BufferedInputStream(inputStream, fileLength);
-				byte[] buffer = new byte[fileLength];
-				bufInputStream.read(buffer, 0, fileLength);
-				bufInputStream.close();
-				ByteBuffer rmtBuf = ByteBuffer.wrap(buffer, 0, fileLength);
-
-				// decoder to go from remote encoding to UTF8
-				Charset rmtCharset = Charset.forName(remoteEncoding);
-				CharsetDecoder rmtDecoder = rmtCharset.newDecoder();
-
-				// convert from the remote encoding
-				CharBuffer decodedBuf = null;
-				decodedBuf = rmtDecoder.decode(rmtBuf);
-				// for conversion to the local encoding
-				Charset charset = Charset.forName(localEncoding);
-				CharsetEncoder encoder = charset.newEncoder();
-				byte[] localBuffer = null;
-				// convert to the specified local encoding
-				ByteBuffer lclBuf = encoder.encode(decodedBuf);
-				localBuffer = lclBuf.array();
-				outStream = new FileOutputStream(file);
 				
-				// use the limit rather than the array length to avoid unwanted nulls
-				outStream.write(localBuffer, 0, lclBuf.limit());
+				int MAX_READ = 10000;
+				if (fileLength <= MAX_READ){ // read the whole file at once
+
+					byte[] buffer = new byte[fileLength];
+					bufInputStream.read(buffer, 0, fileLength);
+					bufInputStream.close();
+					inputStream.close();
+					outStream = new FileOutputStream(file);
+					convertBuffer(buffer, rmtDecoder, fileLength, localEncoding, outStream);
+				}
+				else { // read and write sections of file at a time
+					int inOffset = 0;
+					
+					File altFile = new File(file.getAbsolutePath() + "~"); //$NON-NLS-1$
+					outStream = new FileOutputStream(altFile); // using alternate file because we're writing while reading
+					while (inOffset < fileLength){
+						int readSize = MAX_READ;
+						if (inOffset +  MAX_READ > fileLength){
+							readSize = fileLength - inOffset;
+						}
+						byte[] buffer = new byte[readSize];
+						inputStream.read(buffer, 0, readSize);
+						inOffset += readSize;
+						convertBuffer(buffer, rmtDecoder, readSize, localEncoding, outStream);
+					}
+					inputStream.close();	
+					outStream.close();
+					altFile.renameTo(file);
+				}
+
 			}
 		} catch (Exception e) {
 			// outstream could not be written properly: report
@@ -98,6 +114,27 @@ public class DefaultFileServiceCodePageConverter implements
 				}
 			}
 		}
+	}
+	
+	private void convertBuffer(byte[] buffer, CharsetDecoder rmtDecoder, int readSize, String localEncoding, FileOutputStream outStream) throws Exception{
+		ByteBuffer rmtBuf = ByteBuffer.wrap(buffer, 0, readSize);		
+		
+		// convert from the remote encoding
+		CharBuffer decodedBuf = null;
+		decodedBuf = rmtDecoder.decode(rmtBuf);
+
+		// for conversion to the local encoding
+		Charset charset = Charset.forName(localEncoding);
+		CharsetEncoder encoder = charset.newEncoder();
+		byte[] localBuffer = null;
+		
+		// convert to the specified local encoding
+		ByteBuffer lclBuf = encoder.encode(decodedBuf);
+		localBuffer = lclBuf.array();
+							
+		// use the limit rather than the array length to avoid unwanted nulls
+		int writeSize = lclBuf.limit();
+		outStream.write(localBuffer, 0, writeSize);
 	}
 
 	public boolean isServerEncodingSupported(String remoteEncoding,
